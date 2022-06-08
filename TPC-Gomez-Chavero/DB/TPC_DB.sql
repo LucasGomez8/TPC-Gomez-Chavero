1f8a7f31cf968590bdc4dbc31cdb4d22311664c8
CChave <eduardo.chavero@coltech.com.ar>
2022-06-08 18:39:38 -0300

    Complete SQL query

--- TPC-Gomez-Chavero/DB/TPC_DB.sql
+++ TPC-Gomez-Chavero/DB/TPC_DB.sql
@@ -8,7 +8,13 @@ use TPC
 go
 
 create table Categorias(
-	IDCategoria Bigint primary key identity(1,1),
+	IDCategoria bigint primary key identity(1,1),
+	Descripcion varchar(50) not null
+)
+go
+
+create table TipoProducto(
+	IDTipoProducto bigint primary key identity(1,1),
 	Descripcion varchar(50) not null
 )
 go
@@ -31,8 +37,83 @@ create table Productos(
 	Descripcion varchar(250) not null,
 	IDCategoria bigint foreign key references Categorias(IDCategoria),
 	IDMarca bigint foreign key references Marcas(IDMarca),
+	IDTipoProducto bigint foreign key references TipoProducto(IDTipoProducto),
 	Stock int not null,
 	StockMinimo int not null,
-	Precio money not null,
+	porcentajeVenta smallint not null,
+)
+go
+
+create table TipoUsuario(
+	idTipoUsuario bigint primary key identity (1,1),
+	descripcion varchar(150),
+)
+go
+
+create table Usuarios(
+	idUsuario bigint primary key identity (1,1),
+	nombre varchar(100) not null,
+	apellido varchar(100) not null,
+	dni varchar(10) not null unique,
+	IDTipoUsuario bigint foreign key references TipoUsuario(IDTipoUsuario),
+	contraseña varchar(20) not null,
+	nick varchar(25) not null,
+	fechaNac date not null,
+)
+go
+
+create table Clientes(
+	idCliente bigint primary key identity (1,1),
+	nombre varchar(100) not null,
+	cuitOrDni varchar(15) null unique,
+	fechaNac date not null,
+	telefono varchar(20) unique null,
+	email varchar(100) unique null,
+)
+go
+
+create table TipoDeFactura(
+	idTipoFactura bigint primary key identity(1,1),
+	descripcion varchar(100) not null,
+)
+go
+
+create table Ventas(
+	IDRegistro bigint primary key identity(1,1),
+	NumeroFactura bigint unique,
+	TipoFactura bigint foreign key references TipoDeFactura(idTipoFactura),
+	idVendedor bigint foreign key references Usuarios(idUsuario),
+	idCliente bigint foreign key references Clientes(IDCliente),
+	Fecha date not null,
+	MontoTotal money not null,
+	detalle varchar(max)
+)
+go
+
+create table Compras(
+	IDRegistro bigint primary key identity(1,1),
+	NumeroFactura bigint unique,
+	TipoFactura bigint foreign key references TipoDeFactura(idTipoFactura),
+	IDProveedor bigint foreign key references Proveedores(IDProveedor),
+	IDAdministrador bigint foreign key references Usuarios(IDUsuario),
+	Fecha date not null,
+	MontoTotal money not null,
+	detalle varchar(max)
+)
+go
+
+create table ProductoXCompra(
+	IDRegistro bigint foreign key references Compras(IDRegistro) not null,
+	IDProducto bigint foreign key references Productos(IDProducto) not null,
+	Cantidad bigint not null,
+	precioCompra money not null,
+)
+go
+
+create table ProductoXVenta(
+	IDRegistro bigint foreign key references Ventas(IDRegistro) not null,
+	IDProducto bigint foreign key references Productos(IDProducto) not null,
+	Cantidad bigint not null,
+	precioVenta money not null,
 )
 go
